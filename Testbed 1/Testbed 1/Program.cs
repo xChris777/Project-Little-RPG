fb289339093ca3d6965316c8c80f724bb02e7e58
xChris777 <cmc710@protonmail.com>
2022-06-14 15:30:44 -0400

    initial commit

--- Testbed 1/Testbed 1/Program.cs
+++ Testbed 1/Testbed 1/Program.cs
@@ -29,56 +29,57 @@ namespace Testbed_1
 
             Console.WriteLine("{0}, you're about to venture into an unknown world, filled with mystery and adventure...", name);
 
-            Console.WriteLine("Please tell me - are you a mage, a rogue, or a swordfighter {0}?", name);
+            Console.WriteLine("Please tell me - are you a mage, a rogue, or a swordfighter, or something else entirely?");
+            /*
+            Console.ReadLine();
+            
 
-            string classInput = Console.ReadLine();
+            if (Console.ReadLine().Equals("mage") || Console.ReadLine().Equals("rogue") || Console.ReadLine().Equals("swordfighter"))
 
-            if(classInput.Equals("mage") || classInput.Equals("rogue") || classInput.Equals("swordfighter"))
+                switch (Console.ReadLine())
 
-                switch (classInput)
                 {
-
                     case "mage":
-                    {
-                        Console.WriteLine("Mages are wise. You will be able to use spells to aid you in your travels.");
-                        Console.WriteLine("You've selected mage as your class.");
-                        string charClass = "mage";
-                    }
-                    break;
+                        {
+                            Console.WriteLine("Mages are wise. You will be able to use spells to aid you in your travels.");
+                            Console.WriteLine("You've selected mage as your class.");
+                            string charClass = "mage";
+                        }
+                        break;
 
                     case "rogue":
-                    {
-                        Console.WriteLine("Rogues are sneaky. If there's ever an opportunity to get into somewhere you shouldn't be, you'll be able to capitalize on it.");
-                        Console.WriteLine("You've selected rogue as your class.");
-                        string charClass = "rogue";                        
-                    }
-                    break;
-                    
-                    case "swordfighter":
-                    { 
-                        Console.WriteLine("Swordfighters are strong. During your travels, you may come across interesting ways to use this strength.");
-                        Console.WriteLine("You've selected swordfighter as your class.");
-                        string charClass = "swordfighter";
-                    }
+                        {
+                            Console.WriteLine("Rogues are sneaky. If there's ever an opportunity to get into somewhere you shouldn't be, you'll be able to capitalize on it.");
+                            Console.WriteLine("You've selected rogue as your class.");
+                            string charClass = "rogue";
+                        }
                         break;
-                }   
+
+                    case "swordfighter":
+                        {
+                            Console.WriteLine("Swordfighters are strong. During your travels, you may come across interesting ways to use this strength.");
+                            Console.WriteLine("You've selected swordfighter as your class.");
+                            string charClass = "swordfighter";
+                        }
+                        break;
+                }
+
+            
+
                 
-                else 
-                
                 
 
-            // else while (charClass != "mage" || charClass != "rogue" || charClass != "swordfighter")
-            // need to make a loop, so that if mage, rogue or swordfighter aren't the string inputted, we ask again over and over until one of those 3 options are asked. 
+                    // need to make a loop, so that if mage, rogue or swordfighter aren't the string inputted, we ask again over and over until one of those 3 options are asked. 
 
 
 
 
-
+ */
         }
         
         
 
-        
+       
 
     }
 }
